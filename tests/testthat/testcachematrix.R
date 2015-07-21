c15a8f4d1dad22b88edf2612471c81415abdbc5c
hegold <hegold@users.noreply.github.com>
2015-07-21 23:23:37 +0100

    Matrix getter tests

--- tests/testthat/testcachematrix.R
+++ tests/testthat/testcachematrix.R
@@ -1,10 +1,34 @@
 source("../../cachematrix.R")
 
-test_that("makeCacheMatrix creates object with expected accessors", {
+context("makeCacheMatrix")
+
+makeTestMatrix <- function() { matrix(runif(100), 10, 10) }
+
+test_that("matrix object has expected accessors", {
     m <- makeCacheMatrix()
+
     expect_named(m, c('get', 'set', 'getinverse', 'setinverse'), ignore.order = TRUE)
     expect_is(m$get, 'function')
     expect_is(m$set, 'function')
     expect_is(m$getinverse, 'function')
     expect_is(m$setinverse, 'function')
+})
+
+test_that("matrix object's get returns original matrix", {
+    expected <- makeTestMatrix()
+    m <- makeCacheMatrix(expected)
+
+    result = m$get()
+
+    expect_identical(result, expected)
+})
+
+test_that("matrix object's get returns new matrix after set", {
+    m <- makeCacheMatrix(makeTestMatrix())
+    expected <- makeTestMatrix()
+    m$set(expected)
+
+    result = m$get()
+
+    expect_identical(result, expected)
 })
