ee23b69a7cf37926ab049dfb79a45e9ac73a82f2
hegold <hegold@users.noreply.github.com>
2015-07-21 22:43:57 +0100

    Matrix creation test

--- tests/testthat/testcachematrix.R
+++ tests/testthat/testcachematrix.R
@@ -1,1 +1,10 @@
+source("../../cachematrix.R")
 
+test_that("makeCacheMatrix creates object with expected accessors", {
+    m <- makeCacheMatrix()
+    expect_named(m, c('get', 'set', 'getinverse', 'setinverse'), ignore.order = TRUE)
+    expect_is(m$get, 'function')
+    expect_is(m$set, 'function')
+    expect_is(m$getinverse, 'function')
+    expect_is(m$setinverse, 'function')
+})
